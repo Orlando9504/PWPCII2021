3e05cea57120e01e570bff1d0394991e43de403d
Orlando9504 <orlando9504@gmail.com>
2021-10-13 14:04:58 -0500

    prettier funcionando

--- client/index.js
+++ client/index.js
@@ -1,25 +1,28 @@
-import './stylesheets/style.css'
-import './stylesheets/mystyles.css'
-console.log("Webpack esta trabajando!!");
-//Default parameters ES6/2015
-let show =(m="hola") => {
-    alert(m)
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import './stylesheets/style.css';
+import './stylesheets/mystyles.css';
+
+console.log('Webpack esta trabajando!!');
+// Default parameters ES6/2015
+const show = (m = 'hola') => {
+  alert(m);
 };
 
 show();
 
-function resolveafter25seconds(){
-    return new Promise(resolve=>{
-        setTimeout(()=>{
-            resolve('resolved')
-        },2000);
-    });
+function resolveafter25seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
 }
 
-async function asyncCall(){
-    console.log("Calling an async function");
-    const result = await resolveafter25seconds();
-    console.log(result);
+async function asyncCall() {
+  console.log('Calling an async function');
+  const result = await resolveafter25seconds();
+  console.log(result);
 }
 
 asyncCall();
--- server/app.js
+++ server/app.js
@@ -1,49 +1,53 @@
+/* eslint-disable no-console */
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan'
+import logger from 'morgan';
 
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
 
-//Webpack modulos
+// Webpack modulos
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.dev.config';
+import webpackConfig from '../webpack.config';
 import webpackDevConfig from '../webpack.dev.config';
 
-//Consultar el modo en que se esta ejecutando el programa
+// Consultar el modo en que se esta ejecutando el programa
 const env = process.env.NODE_ENV || 'development';
 
-//Se crea la aplicacion express
-var app = express();
+// Se crea la aplicacion express
+const app = express();
 
-//Verificando el modo de ejecución de la aplicación 
+// Verificando el modo de ejecución de la aplicación
 
-if(env === 'development'){
+if (env === 'development') {
   console.log('> Execting in Development Mode: Webpack Hot Reloading');
-//paso 1 = Agregando la ruta de HMR
-//reload habilita la recarga del frontend cuando hay cambios en el codigo fuente
-//fuente del frontend
-//timeout=1000: Tiempo de espera entre recarga y recarga de la pagina
-webpackConfig.entry=['webpack-hot-middleware/client?reload=true&timeout=1000',webpackConfig.entry];
-//Paso 2. Agregamos el plugin
-webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  //  timeout=1000: Tiempo de espera entre recarga y recarga de la pagina
+  //  paso 1 = Agregando la ruta de HMR
+  //  reload habilita la recarga del frontend cuando hay cambios en el codigo fuente
+  //  fuente del frontend
+  webpackConfig.entry = [
+    'webpack-hot-middleware/client?reload=true&timeout=1000',
+    webpackConfig.entry,
+  ];
+  // Paso 2. Agregamos el plugin
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 
-//Paso 3. Crear el compilador de webpack
-const compiler = webpack(webpackConfig);
+  // Paso 3. Crear el compilador de webpack
+  const compiler = webpack(webpackConfig);
 
-//paso 4 Agregamdo el Middleware a la cadena de middlewares de nuestra aplicacion
-app.use(WebpackDevMiddleware(compiler,{
-  publicPath: webpackDevConfig.output.publicPath
-}));
-
-//paso 5. Agregando el webpack hot middleware
-app.use(WebpackHotMiddleware(compiler))
-}
-else{
+  // paso 4 Agregamdo el Middleware a la cadena de middlewares de nuestra aplicacion
+  app.use(
+    WebpackDevMiddleware(compiler, {
+      publicPath: webpackDevConfig.output.publicPath,
+    }),
+  );
+  // paso 5. Agregando el webpack hot middleware
+  app.use(WebpackHotMiddleware(compiler));
+} else {
   console.log('>Executing in Production Mode...');
 }
 
@@ -55,18 +59,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'..','public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
